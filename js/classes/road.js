4915bfcfabfd83398109f54fafcdd625daf81b37
KarloParagas <karloparagas@gmail.com>
2020-04-08 22:23:09 -0700

    Added road class

---
+++ js/classes/road.js
@@ -0,0 +1,17 @@
+//Container is a collection of objects that can be treated as a single object 
+//Each item in a container moves at the same time
+class Road extends Phaser.GameObjects.Container {
+    constructor(config) {
+        super(config.scene); //Pass the scene to Phaser.GameObjects.Container's constructor
+        this.scene = config.scene; //Makes a reference to the scene
+        this.back = this.scene.add.image(0, 0, 'road'); //Places the image of the road in this class
+        this.add(this.back); //Places the road inside the container
+        this.scene.add.existing(this); //Add it to the scene
+
+        //Scale the road/"back" to 50% of the screen size, so 2 can fit
+        this.back.displayWidth = game.config.width * 0.5;
+
+        //Makes sure the scale is proportional
+        this.back.scaleY = this.back.scaleX;
+    }
+}
