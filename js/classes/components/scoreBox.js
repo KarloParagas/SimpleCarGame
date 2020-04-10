cc10fbcb7c1fa43a4ee88deb909844c8c1ff784e
KarloParagas <karloparagas@gmail.com>
2020-04-10 11:41:20 -0700

    Added scorebox class that displays scores

---
+++ js/classes/components/scoreBox.js
@@ -0,0 +1,19 @@
+class ScoreBox extends Phaser.GameObjects.Container {
+    constructor(config) {
+        super(config.scene); //Call the scene container's constructor
+        this.scene = config.scene; //Reference the scene
+
+        this.text1 = this.scene.add.text(0, 0, "SCORE: 0"); //Add a text box to hold the score
+        this.text1.setOrigin(0.5, 0.5); //Put the text in the senter of the canvas
+        this.add(this.text1); //Add text/scorebox to the container
+
+        this.scene.add.existing(this); //Add it to the scene
+
+        emitter.on(G.SCORE_UPDATED, this.scoreUpdated, this); //This will "listen" for an event from the model to be emitted
+    }
+
+    scoreUpdated() {
+        this.text1.setText("SCORE:" + model.score); //Update the text with the current score of the game
+
+    }
+}
