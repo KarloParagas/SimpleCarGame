fa5d630e60ef6ad1e529d0692502678ad7c3a79c
KarloParagas <karloparagas@gmail.com>
2020-04-09 19:58:46 -0700

    Fixed car spritesheet preload

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -12,7 +12,7 @@ class SceneMain extends Phaser.Scene { //All properties and functions of Phaser.
         //Load images
         this.load.image('road', 'images/road.jpg');
         this.load.image('line', 'images/line.png');
-        this.load.image('cars', 'images/cars.png', {framewidth: 60, frameHeight: 126});
+        this.load.spritesheet('cars', 'images/cars.png', {frameWidth: 60, frameHeight: 126});
     }
 
     //This function is for defining objects
